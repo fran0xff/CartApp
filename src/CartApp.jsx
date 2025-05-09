c58cb7fad9ca009e89e9fceab8446e95bccf530f
fran <franciscojavierarevalo@hotmail.com>
2025-05-09 20:44:06 +0200

    feat: implement product catalog and cart functionality in CartApp

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,110 +1,74 @@
+import { useState } from "react";
+import { CartView } from "./componets/CartView"
+import { CatalogView } from "./componets/CatalogView"
 
+const initialCartItems = [
+    // {
+    //     product: {
+
+    //     },
+    //     quantity: 1,
+    // }
+];
 
 export const CartApp = () => {
+
+    const [cartItems, setCartItems] = useState(initialCartItems);
+
+    const handlerAddProductCart = (product) => {
+
+        const hasItem = cartItems.find((item) => item.product.id === product.id);
+        if (hasItem) {
+            // setCartItems([
+            //     ...cartItems.filter((item) => item.product.id !== product.id),
+            //     {
+            //         product: product,
+            //         quantity: hasItem.quantity + 1,
+            //     }
+            // ])
+            setCartItems(
+                cartItems.map((item) => {
+                if (item.product.id === product.id) {
+                    return {
+                        ...item,
+                        quantity: item.quantity + 1,
+                    }
+                }
+                return item;
+            }))
+        } else {
+            setCartItems([
+                ...cartItems,
+                {
+                    product: product,
+                    quantity: 1,
+                }
+            ]);  
+        }
+            
+    }
+const handlerDeleteProductCart = (id) => {
+    setCartItems([
+        ...cartItems.filter((item) => item.product.id !== id)
+    ])
+}
+    
+
+
     return (
         <>
 
             <div className="container">
-
                 <h3>Cart App</h3>
-                <div className="row">
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Teclado Mecanico RGB</h5>
-                                <p className="card-text">Teclado Mecanico con luces RGB switches cherry red</p>
-                                <p className="card-text">100 €</p>
-                                <button className="btn btn-primary">Añadir al carro</button>
-                            </div>
+                <CatalogView handler={ handlerAddProductCart } />
 
-                        </div>
+                { cartItems?.length <= 0 || (
+                    <div className="my-4 w-50">
+                        <CartView items={ cartItems } handlerDelete={ handlerDeleteProductCart } />
                     </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Samsung Smart TV</h5>
-                                <p className="card-text">Teclado Mecanico con luces RGB switches cherry red</p>
-                                <p className="card-text">1000 €</p>
-                                <button className="btn btn-primary">Añadir al carro</button>
-                            </div>
+                )}
+                
 
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Auriculares Bluethooth Sony</h5>
-                                <p className="card-text">Teclado Mecanico con luces RGB switches cherry red</p>
-                                <p className="card-text">180 €</p>
-                                <button className="btn btn-primary">Añadir al carro</button>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Memoria Corsair 32GB DDR5</h5>
-                                <p className="card-text">Teclado Mecanico con luces RGB switches cherry red</p>
-                                <p className="card-text">125 €</p>
-                                <button className="btn btn-primary">Añadir al carro</button>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">CPU Intel Core i7 12600K</h5>
-                                <p className="card-text">Teclado Mecanico con luces RGB switches cherry red</p>
-                                <p className="card-text">300 €</p>
-                                <button className="btn btn-primary">Añadir al carro</button>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Asus Nvidia RTX 4070</h5>
-                                <p className="card-text">Teclado Mecanico con luces RGB switches cherry red</p>
-                                <p className="card-text">450 €</p>
-                                <button className="btn btn-primary">Añadir al carro</button>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="my-4 w-50">
-                    <h3>Carro de compras</h3>
-                    <table className="table table-hover table-striped">
-                        <thead>
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Precio</th>
-                                <th>Cantidad</th>
-                                <th>Total</th>
-                                <th>Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>nombre</td>
-                                <td>Precio</td>
-                                <td>Cantidad</td>
-                                <td>Total</td>
-                                <td>eliminar</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colSpan="3" className="text-end fw-bold">Total</td>
-                                <td colSpan="2" className="text-start fw-bold">12345</td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
             </div>
         </>
     )
---
+++ src/componets/CartView.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+
+export const CartView = ({ handlerDelete, items }) => {
+
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        setTotal(
+            items.reduce((acc, item) => acc + item.product.price * item.product.quantity, 0)
+        );
+    }, [items])
+
+
+
+    const onDeleteProduct = (id) => {
+        handlerDelete(id);
+    };
+
+    return (
+        <>
+
+            <h3>Carro de compras</h3>
+            <table className="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Total</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map(item => (
+                        <tr key={item.product.id}>
+                            <td>{item.product.name}</td>
+                            <td>{item.product.price}</td>
+                            <td>{item.quantity}</td>
+                            <td>{item.quantity * item.product.price}</td>
+                            <td><button
+                                onClick={() => onDeleteProduct(item.product.id)}
+                                className="btn btn-danger">Eliminar</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="3" className="text-end fw-bold">Total</td>
+                        <td colSpan="2" className="text-start fw-bold">{total}</td>
+                    </tr>
+                </tfoot>
+            </table>
+
+        </>
+    )
+}
